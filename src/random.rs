d106ef97fc6305ddaf4c60c62eaa81a83f8e043d
vlad-boroda <enity.w@gmail.com>
2019-02-20 21:32:02 +0300

    - random struct

--- src/random.rs
+++ src/random.rs
@@ -1,24 +1,33 @@
-use std::collections::HashSet;
 use rand::prelude::*;
+use std::collections::HashSet;
 
 struct Random {
     cache: HashSet<u8>,
-    rng: ThreadRng
+    rng: ThreadRng,
 }
 
 impl Random {
     fn new() -> Random {
         Random {
             cache: HashSet::with_capacity(9),
-            rng: thread_rng()
+            rng: thread_rng(),
         }
     }
 
-    fn get_new(&mut self) -> bool {
-        let mut generated = self.rng.gen_range(1, 9);
-        while self.cache.insert(generated) == false {
+    fn get_new(&mut self) -> Option<u8> {
+        if self.cache.len() == 9 {
+            None
+        } else {
+            let mut generated = self.rng.gen_range(1, 10);
+            while !self.cache.insert(generated) {
+                generated = self.rng.gen_range(1, 10);
+            }
+            Some(generated)
         }
-        true
+    }
+
+    fn reset(&mut self) {
+        self.cache.clear();
     }
 }
 
@@ -27,12 +36,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_get_new() {
+    fn test_get_new_full() {
         let mut r = Random::new();
-        for _ in 0..8 {
+        for _ in 0..9 {
             r.get_new();
         }
-        dbg!(&r.cache);
-        assert_eq!(false, r.get_new());
+        assert_eq!(None, r.get_new());
+    }
+
+    #[test]
+    fn test_get_new_after_reset() {
+        let mut r = Random::new();
+        for _ in 0..9 {
+            r.get_new();
+        }
+        r.reset();
+        assert_ne!(None, r.get_new());
     }
 }
