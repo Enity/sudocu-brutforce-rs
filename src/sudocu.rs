93b7532ac6c1bce637054a36ff357269aae7e785
vlad-boroda <enity.w@gmail.com>
2019-02-22 18:24:44 +0300

    finish indexesmap

--- src/sudocu.rs
+++ src/sudocu.rs
@@ -3,8 +3,8 @@
 pub struct Sudocu {
     map: [u8; 81],
     side_length: usize,
-    backtrack_step: u8,
-    last_backtrack_index: u8,
+    backtrack_step: usize,
+    last_backtrack_index: usize,
     pub indexes_map: [([usize; 9], [usize; 9], [usize; 9]); 81],
 }
 
@@ -27,10 +27,34 @@ impl Sudocu {
 
             // calculate row indexes
             start_pos = (i / self.side_length) * self.side_length;
-
             for b in 0..self.side_length {
                 self.indexes_map[i].0[b] = start_pos;
                 start_pos += 1;
+            }
+
+             // calculcate column indexes
+            start_pos = i % self.side_length;
+            for b in 0..self.side_length {
+                self.indexes_map[i].1[b] = start_pos;
+                start_pos += self.side_length;
+            }
+
+            // calculate square indexes
+            let x = i % self.side_length;
+            let y = i / self.side_length;
+            let square_side = 3usize;
+
+            let x_startpos = x / square_side * square_side;
+            let y_startpos = y / square_side * square_side;
+            
+            start_pos = x_startpos + self.side_length * y_startpos;
+
+            for b in 0..self.side_length {
+                self.indexes_map[i].2[b] = start_pos;
+                start_pos += 1;
+                if start_pos % square_side == 0 {
+                    start_pos += square_side * 2;
+                }
             }
         }
     }
